dc0683ae0a34eea14c0ee35ef2255bb7cafccab4
vagusx <vagusxl@gmail.com>
2023-05-09 22:21:26 +0800

    scripts: modify terser optins to keep classnames

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,16 +1,30 @@
 // production config
 const { merge } = require('webpack-merge');
 const { resolve } = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
-  // mode: 'production',
-  mode: 'development',
+  mode: 'production',
+  // mode: 'development',
   output: {
     filename: 'js/bundle.[contenthash].min.js',
     path: resolve(__dirname, '../../dist'),
     publicPath: '/',
   },
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+      '...',
+    ],
+  }
 });
